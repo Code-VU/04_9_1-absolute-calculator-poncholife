cb7e31fe11414477f953ef39a0395bf5e9e77d42
Jonathan Willis <Jonathan.Willis@vu.com>
2023-02-16 22:00:37 -0600

    Completed all on my own! (without looking at the solution first)

--- oddAbsolute.py
+++ oddAbsolute.py
@@ -1,7 +1,20 @@
 def calculateAbsolute():
     
     # This first line is provided for you
-    in_num  = input("Enter a number: ")
+    in_num = int(input("Enter a number: "))
+    n = in_num
+    #print(n)
+    x = n - 21
+    #print(x)
+    import math 
+    abs_x = math.fabs(x)
+    if n > 21:
+        z = abs_x * 2
+    else:
+        z = abs_x
+    z = int(z)
+    print('Result:', z)
+
     # end assignment
 
 ## If you want to test locally run > python payCalculator.py
